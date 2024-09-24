e80bbed81b048fceeef4219eb486228dd1ee5d1d
Kevin Bett <prodigy4614@gmail.com>
2024-09-24 20:23:40 +0300

    added fuctions to calculate distance and fare price.

--- index.js
+++ index.js
@@ -4,12 +4,26 @@ function distanceFromHqInBlocks(block){
 return Math.abs(block - headquarters);
 }
 
-function distanceFromHqInFeet(block){
-const blocklength = 264;
-const blocks = distanceFromHqInBlocks(block);
-return blocks * blocklength;
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
 }
 
-distanceTravelledInFeet(){
-    
+function distanceTravelledInFeet(start, destination){
+return Math.abs(start - destination)*264;
 }
+
+function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(start - destination)*264;
+    if (distance<=400){
+        return 0;
+    }
+    else if(distance > 400 && distance <= 2000){
+        return (distance - 400)*0.02;
+    }
+    else if(distance > 2000 && distance <= 2500){
+        return 25;
+    }
+    else if(distance > 2500){
+        return "cannot travel that far";
+    }
+  }
