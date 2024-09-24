b4e21ea2f58acb175890b8e876f527013cacabe4
Kevin Bett <prodigy4614@gmail.com>
2024-09-24 20:23:25 +0300

    added fuctions to calculate distance and fare

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+const headquarters = 42;
+function distanceFromHqInBlocks(block){
+return Math.abs(block - headquarters);
+}
+
+function distanceFromHqInFeet(block){
+const blocklength = 264;
+const blocks = distanceFromHqInBlocks(block);
+return blocks * blocklength;
+}
+
+distanceTravelledInFeet(){
+    
+}
